e1117cff011a69c8b6556636ff994bf8d337d9c9
leerkenis <leerkenis@gmail.com>
2024-11-12 16:41:17 -0300

    Add files via upload

---
+++ firebaseauth.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC8tF0tyQ8Z1g2sMrDvk3uq3uvVK1ylAsE",
+  authDomain: "bijledits.firebaseapp.com",
+  projectId: "bijledits",
+  storageBucket: "bijledits.firebasestorage.app",
+  messagingSenderId: "558075309542",
+  appId: "1:558075309542:web:bdf7124653fe31fd42fa27"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
